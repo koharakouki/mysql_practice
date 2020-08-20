92c6ed3f2333f3c2ce17612b921d476769cdeb17
koharakouki <ko.kohara02@gmail.com>
2020-08-21 02:26:52 +0900

    [fin] chapter04

--- mysql_practice.sql
+++ mysql_practice.sql
@@ -67,7 +67,7 @@ $ SELECT DATABASE();
 
 $ CREATE TABLE テーブル名(カラム名1 データ型1, カラム名2 データ型2);
 
-$ CREATE TABLE db1(bang VARCHAR(10), nama VARCHAR(10), tosi INT);
+$ CREATE TABLE tb1(bang VARCHAR(10), nama VARCHAR(10), tosi INT);
 Query OK, 0 rows affected (0.04 sec)
 
 
@@ -81,6 +81,81 @@ $ SHOW TABLES;
 | db1           |
 +---------------+
 1 row in set (0.00 sec)
+
+
+●useでデータベースを選択した状態で、他のデータベースにアクセスする----------------------------------------------------
+
+$ SELECT * FROM db2.table;
+
+
+
+●テーブルのカラム構造の確認-------------------------------------------------------
+
+$ DESC テーブル名;
+
+$ DESC tb1;
++-------+-------------+------+-----+---------+-------+
+| Field | Type        | Null | Key | Default | Extra |
++-------+-------------+------+-----+---------+-------+
+| bang  | varchar(10) | YES  |     | NULL    |       |
+| nama  | varchar(10) | YES  |     | NULL    |       |
+| tosi  | int(11)     | YES  |     | NULL    |       |
++-------+-------------+------+-----+---------+-------+
+3 rows in set (0.01 sec)
+
+※Fieldがカラム名、Typeがデータ型
+
+
+●テーブルにデータを挿入する------------------------------------------------------
+
+$ INSERT INTO テーブル名 VALUES(データ1, データ2, データ3,.....);
+
+$ INSERT INTO tb1 VALUES('A101', '佐藤', 40);
+Query OK, 1 row affected (0.00 sec)
+
+$ INSERT INTO tb1 VALUES('A102', '高橋', 28);
+Query OK, 1 row affected (0.00 sec)
+
+
+●カラムを指定してデータを挿入する------------------------------------------------------
+
+$ INSERT INTO テーブル名 (カラム名1, カラム名2) VALUES(データ1, データ2);
+
+$ INSERT INTO tb1 (tosi, nama, bang) VALUES(23, '渡辺', 'A104');
+Query OK, 1 row affected (0.01 sec)
+
+$ INSERT INTO tb1 (tosi, nama, bang) VALUES(35, '西沢', 'A105');
+Query OK, 1 row affected (0.01 sec)
+
+
+●一度にデータを挿入する------------------------------------------------------
+
+$ INSERT INTO テーブル名 (カラム名1, カラム名2) VALUES(データ1, データ2), (データ1, データ2), (データ1, データ2);
+
+
+●データを表示する------------------------------------------------------
+
+$ SELECT カラム名1, カラム名2 FROM テーブル名;
+
+$ SELECT bang, nama FROM tb1;
+
+レコード全部表示させるときはこっち
+
+$ SELECT * FROM tb1;
+
++------+--------+------+
+| bang | nama   | tosi |
++------+--------+------+
+| A101 | 佐藤   |   40 |
+| A102 | 高橋   |   28 |
+| A103 | 中川   |   20 |
+| A104 | 渡辺   |   23 |
+| A105 | 西沢   |   35 |
++------+--------+------+
+5 rows in set (0.00 sec)
+
+
+
 
 
 
